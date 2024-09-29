46cdcb319354975ca3f729f2533ec3d928a8f1dd
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-09-29 14:02:04 -0300

    noticias mobile v1

--- script/script.js
+++ script/script.js
@@ -13,3 +13,34 @@ const myObserver = new IntersectionObserver((entries)=>{
 elements.map((el)=>{
     myObserver.observe(el)
 })
+function initSwiper() {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true, // Habilita o loop infinito dos slides
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false,
+        // },
+        grabCursor: true,
+        spaceBetween: 15, // Espaçamento entre os slides
+        slidesPerView: 1, // Quantidade de slides visíveis por vez
+        centeredSlides: true, // Centraliza o slide ativo
+    })
+    swiper.on('reachBeginning', function () {
+        swiper.allowSlidePrev = false; // Desativa a navegação para a esquerda
+    });
+    swiper.on('reachEnd', function () {
+        swiper.allowSlidePrev = true; // Permite a navegação para a esquerda
+    });
+}
+function checkScreenSize() {
+    if (window.matchMedia('(max-width: 980px)').matches) {
+        // Inicializa o Swiper se a largura da tela for igual ou menor que 980px
+        initSwiper();
+    }
+}
+checkScreenSize()
+window.addEventListener('resize', checkScreenSize)
